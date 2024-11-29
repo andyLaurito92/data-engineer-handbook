7d2be5745d3294a7498101a4308ce31b662c5729
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-11-29 10:44:53 +0100

    Working on scd for actors

--- bootcamp/materials/1-dimensional-data-modeling/homework/homework_solution.sql
+++ bootcamp/materials/1-dimensional-data-modeling/homework/homework_solution.sql
@@ -72,3 +72,60 @@ on y.actorid = t.actorid;
 
 select * from actors;
 select * from actors where actor = 'Alain Delon' and current_year = 1970;
+
+
+--- 3. DDL for actors_history_scd
+
+drop table actors_history_scd;
+create table actors_history_scd (
+	actorid text,
+	actor text,
+	is_active bool,
+	quality_class quality_class,
+	start_year INTEGER,
+	end_year INTEGER,
+	current_year INTEGER,
+	
+	primary key (actorid, start_year)
+);
+
+insert into actors_history_scd
+with previous_dimension_values as (
+	select 
+		*,
+		lag(is_active, 1) over (partition by actorid order by current_year) as prev_is_active,
+		lag(quality_class, 1) over (partition by actorid order by current_year) prev_quality_class
+	from actors
+	where current_year <= 1976
+),
+changed_records_indicators as (
+	select *,
+		case 
+			when prev_is_active <> is_active then 1 
+			when quality_class <> prev_quality_class then 1
+			else 0 
+		end as changed_indicator
+	from previous_dimension_values 
+),
+streak_identifier as (
+	select 
+		*,
+		sum(changed_indicator) over (partition by actorid order by current_year) as streak
+	from changed_records_indicators
+), 
+changed_record as (
+	select 
+		actorid,
+		actor,
+		is_active,
+		quality_class,
+		min(current_year) as start_year,
+		max(current_year) as end_year,
+		1976 as current_year
+	from streak_identifier
+	group by actorid, actor, is_active, quality_class, streak
+	order by actor, start_year
+)
+
+select * from changed_record;
+
